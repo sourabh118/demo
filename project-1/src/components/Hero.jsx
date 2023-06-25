a7641bc6c20a5be37235f2fb8ad578556997aa3a
sourabh <sourabhdeveloper8@gmail.com>
2023-06-24 17:56:21 -0700

    initial commit

---
+++ project-1/src/components/Hero.jsx
@@ -0,0 +1,33 @@
+
+
+
+const HeroSection=()=>{
+return(
+    <>
+<main className="hero container">
+
+    <div className="hero-content">
+        <h1>Run Faster,Run Better with Our Premium Nike </h1>
+    
+        <p>There are many different types of shoes. Athletic shoes, for example, are lightweight to make running, walking or jogging easier. The sole of the shoe is also softer.</p>
+        
+        <div className="hero-btn">
+        <button>Buy Now</button>
+        <button className="second-button">Order Now</button>
+    </div>
+
+    </div>
+
+    
+
+    
+    
+   
+    <div className="hero-image">
+        <img src="./images/shoes1.png" alt="car-image"/>
+    </div>
+</main>
+    </>
+)
+}
+export default HeroSection
---
+++ project-1/src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+
+const Navigation = () => {
+return (
+<nav className="container">
+<div className="logo">
+<img src="/images/001.jpg" alt="loclgo"/>
+</div>
+<ul>
+<li href="#">Home</li>
+<li href="#">About</li>
+<li href="#">Service</li>
+<li href="#">Contact</li>
+</ul>
+<button>Sign Up</button>
+</nav>
+    
+  )
+}
+
+export default Navigation
+
